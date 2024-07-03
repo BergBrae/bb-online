1428328ceac2e608673c35cf4ff5a073658c29bb
Brady Berg <bergbrae@msu.edu>
2024-07-02 22:06:04 -0400

    more accurate getRandomPosition

--- src/utils.js
+++ src/utils.js
@@ -5,8 +5,10 @@ export const getRandomIcons = (icons, count) => {
 };
 
 export const getRandomPosition = () => {
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
   return {
-    top: `${Math.random() * 80 + 10}%`,
-    left: `${Math.random() * 80 + 10}%`
+    top: `${Math.random() * (vh - 100)}px`, // Assuming 100px as the maximum height of the icon
+    left: `${Math.random() * (vw - 100)}px` // Assuming 100px as the maximum width of the icon
   };
 };
