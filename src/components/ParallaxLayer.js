437636637503893950200e5da81c3c149874c202
Brady Berg <bergbrae@msu.edu>
2024-07-02 21:51:09 -0400

    render icons to acutal size

--- src/components/ParallaxLayer.js
+++ src/components/ParallaxLayer.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 
 const ParallaxLayer = ({ src, speed, style }) => {
+  const [size, setSize] = useState({ width: 100, height: 100 });
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    if (imgRef.current) {
+      const { naturalWidth, naturalHeight } = imgRef.current;
+      setSize({ width: naturalWidth, height: naturalHeight });
+    }
+  }, [src]);
+
   return (
     <Parallax speed={speed} style={style}>
-      <img src={src} alt="" style={{ width: '100px', height: '100px' }} />
+      <img
+        ref={imgRef}
+        src={src}
+        alt=""
+        style={{
+          width: `${size.width}px`,
+          height: `${size.height}px`,
+        }}
+      />
     </Parallax>
   );
 };
