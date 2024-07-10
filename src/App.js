f76209e601f5ba4567163321419426dd725f361f
Brady Berg <bergbrae@msu.edu>
2024-07-10 17:16:04 -0400

    Remove parallax bullshit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ParallaxProvider } from 'react-scroll-parallax';
 import Header from './components/Header';
 import Home from './components/Home';
 import LifeSoFar from './components/LifeSoFar';
 import WordsOfEncouragement from './components/WordsOfEncouragement';
-import ParallaxLayer from './components/ParallaxLayer';
 
 function App() {
   const [backgroundImage, setBackgroundImage] = useState(null);
@@ -18,35 +16,17 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <ParallaxProvider>
-          <div style={{ position: 'relative'}}>
-            {backgroundImage && (
-              <ParallaxLayer
-                src={backgroundImage}
-                speed={-10} // Negative value to scroll slower
-                style={{
-                  position: 'absolute',
-                  width: '100%',
-                  height: '100%',
-                  top: 0,
-                  left: 0,
-                  zIndex: 1,
-                }}
-              />
-            )}
-            <div style={{ position: 'relative', zIndex: 10 }}>
-              <div style={{ position: 'sticky', top: 0, zIndex: 20, backgroundColor: 'white' }}>
-                <Header />
-              </div>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/life-so-far" element={<LifeSoFar />} />
-                <Route path="/words-of-encouragement" element={<WordsOfEncouragement />} />
-              </Routes>
-            </div>
+      <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed', minHeight: '100vh' }}>
+        <div style={{ position: 'relative', zIndex: 10 }}>
+          <div style={{ position: 'sticky', top: 0, zIndex: 20, backgroundColor: 'white' }}>
+            <Header />
           </div>
-        </ParallaxProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/life-so-far" element={<LifeSoFar />} />
+            <Route path="/words-of-encouragement" element={<WordsOfEncouragement />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
