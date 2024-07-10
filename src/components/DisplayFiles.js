5cfb06aabaae11e461c1d5644d0c458c28c8ff09
Brady Berg <bergbrae@msu.edu>
2024-07-09 20:59:45 -0400

    chore: Add modal for file preview in DisplayFiles component

--- src/components/DisplayFiles.js
+++ src/components/DisplayFiles.js
@@ -1,13 +1,14 @@
-// src/components/DisplayFiles.js
 import React, { useEffect, useState } from 'react';
 import { storage } from '../firebase';
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import Masonry from 'react-masonry-css';
-import { Container } from 'react-bootstrap';
+import { Container, Modal } from 'react-bootstrap';
 import '../App.css';  // Ensure this imports the CSS styles
 
 const DisplayFiles = ({ folder }) => {
     const [files, setFiles] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
 
     useEffect(() => {
         const fetchFiles = async () => {
@@ -19,6 +20,16 @@ const DisplayFiles = ({ folder }) => {
 
         fetchFiles();
     }, [folder]);
+
+    const handleShow = (file) => {
+        setSelectedFile(file);
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+        setSelectedFile(null);
+    };
 
     const breakpointColumnsObj = {
         default: 3,
@@ -33,7 +44,7 @@ const DisplayFiles = ({ folder }) => {
                 columnClassName="my-masonry-grid_column"
             >
                 {files.map((url, index) => (
-                    <div key={index} style={{ margin: '10px' }}>
+                    <div key={index} style={{ margin: '10px' }} onClick={() => handleShow(url)}>
                         {url.match(/\.(jpeg|jpg|gif|png)/) ? (
                             <img src={url} alt={`file-${index}`} style={{ width: '100%', display: 'block' }} />
                         ) : (
@@ -44,6 +55,21 @@ const DisplayFiles = ({ folder }) => {
                     </div>
                 ))}
             </Masonry>
+
+            <Modal show={showModal} onHide={handleClose} size="lg" centered>
+                {/* <Modal.Header closeButton>
+                    <Modal.Title>File Preview</Modal.Title>
+                </Modal.Header> */}
+                <Modal.Body>
+                    {selectedFile && selectedFile.match(/\.(jpeg|jpg|gif|png)/) ? (
+                        <img src={selectedFile} alt="Selected file" style={{ width: '100%' }} />
+                    ) : (
+                        <video width="100%" controls>
+                            <source src={selectedFile} type="video/mp4" />
+                        </video>
+                    )}
+                </Modal.Body>
+            </Modal>
         </Container>
     );
 };
