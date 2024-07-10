d6c4501980ead234e78fcca0fa68a891dfe840f3
Brady Berg <bergbrae@msu.edu>
2024-07-09 21:02:07 -0400

    refactor: Add responsive behavior to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,33 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Header = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand className='ms-3'>Brenda's 60th</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <LinkContainer to="/">
-          <Nav.Link>Home</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/life-so-far">
-          <Nav.Link>Life So Far</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/words-of-encouragement">
-          <Nav.Link>Words of Encouragement</Nav.Link>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleSelect = () => {
+    setExpanded(false);
+  };
+
+  return (
+    <Navbar bg="light" expand="lg" expanded={expanded} onToggle={setExpanded}>
+      <Navbar.Brand className='ms-3'>Brenda's 60th</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(exp => !exp)} />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto" onSelect={handleSelect}>
+          <LinkContainer to="/" onClick={handleSelect}>
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/life-so-far" onClick={handleSelect}>
+            <Nav.Link>Life So Far</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/words-of-encouragement" onClick={handleSelect}>
+            <Nav.Link>Words of Encouragement</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
